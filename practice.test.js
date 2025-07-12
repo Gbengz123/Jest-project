0f6654eba72a73e07f48959c0680c77019a54117
Danny <danny@Daniels-MacBook-Air.local>
2025-07-12 10:43:28 +0100

    Add test

--- practice.test.js
+++ practice.test.js
@@ -10,3 +10,16 @@ test('reverseString', () => {
   expect(reverseString("hello")).toBe("olleh")
 })
 
+test('calculator', () => {
+  calc = {
+    add: (num1, num2) => num1 + num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+    subtract: (num1, num2) => num1 - num2,
+  }
+  
+  expect(calc.add(1,2)).toBe(3)
+  expect(calc.multiply(4,2)).toBe(8)
+  expect(calc.divide(8,4)).toBe(2)
+  expect(calc.subtract(1,2)).toBe(-1)
+})
